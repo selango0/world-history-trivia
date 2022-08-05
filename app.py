cc00398d9d4077bbc1bdd5f9478318a5058c2e1f
sys363 <sys363@jefferson.edu>
2022-08-05 14:34:24 -0400

    trying to set up psql

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 import os
 import sys
 from flask import Flask, redirect, render_template, request, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+# from config import host, port, database, user, password
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{user}:{password}@{host}/{database}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:b61a657a49bded402e7b973876160162a77aa8ce004557a926dbc12778209d84@localhost/d4kpadureecp54"
+db = SQLAlchemy(app)
 
-print("Hello World", file=sys.stderr)
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Event: {self.description}"
+
+    def __init__(self, description, first_name, last_name):
+        self.description = description
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+@app.route('/')
+def home():
+    return "Hey this is the home page"
 
 def create_app():
     return app
